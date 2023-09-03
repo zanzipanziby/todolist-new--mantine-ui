b5152b5482b7ef48808d189f84b0e420812ae1c8
zanzipanziby <zanzipanziby@gmail.com>
2023-09-04 00:26:20 +0300

    feat: create and connect redux-store

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
-
 import { MantineProvider } from '@mantine/core'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
 
 import App from './App.tsx'
-
+import { store } from './services/store.ts'
 import './index.scss'
 
 createRoot(document.getElementById('root')!).render(
-  <MantineProvider withGlobalStyles withNormalizeCSS theme={{ colorScheme: 'dark' }}>
-    <App />
-  </MantineProvider>
+  <Provider store={store}>
+    <MantineProvider withGlobalStyles withNormalizeCSS theme={{ colorScheme: 'dark' }}>
+      <App />
+    </MantineProvider>
+  </Provider>
 )
