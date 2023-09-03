427d426adbd2141516d255b99f013f89bd3e5c1f
zanzipanziby <zanzipanziby@gmail.com>
2023-09-03 10:45:37 +0300

    init app with storybook

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+import { StrictMode } from 'react'
+
+import './styles/index.scss'
+
+import { createRoot } from 'react-dom/client'
+
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
